a369cefca0be04f256fd091416cfb5d79659ade4
why <1103220942@qq.com>
2020-03-23 19:41:52 +0800

    support Service

---
+++ repo/jcart-support-back/src/test/java/com/why/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.why.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
